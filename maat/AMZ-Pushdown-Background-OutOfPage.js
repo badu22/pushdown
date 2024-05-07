4267de2f54b06f98182322efbac0727f31a89b88
badu22 <marko.badurina@gmail.com>
2024-05-07 13:26:29 +0200

    main files for test

---
+++ maat/AMZ-Pushdown-Background-OutOfPage.js
@@ -0,0 +1,23 @@
+<script>
+    var _zbfpq = _zbfpq || [];
+    var cfg = {
+        debug: true
+        ,filename: '[%Creative%]'
+        ,filename_poster: '[%Creativeposter%]'
+        ,filename_webm: '[%Creativewebm%]'
+        ,filename_ogv: '[%Creativeogv%]'
+        ,width: '100%'
+        ,height: '100'
+        //,headerHeight: '130'
+        ,clickTrack: '%%CLICK_URL_UNESC%%[%ClickThroughURL%]'
+        ,impressor: '[%ImpressionTrackerURL%]'
+    };
+    _zbfpq.push(cfg);
+    (function() {
+        var e = document.createElement('script'); e.type = 'text/javascript'; e.async = true;
+        e.src = '%%VIEW_URL_UNESC%%https://media.kurir.rs/banner/amz/js/zbf-pushdown.min5-test.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
+    })();
+</script>
+
+[%ImpressionTrackerScript%]
---
+++ maat/pushDownV2.js
@@ -0,0 +1,26 @@
+/* eslint-disable func-names */
+/* eslint-disable no-use-before-define */
+/* eslint-disable wrap-iife */
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+const _zbfpq = _zbfpq || [];
+const cfg = {
+    debug: false,
+    filename: 'https://media.kurir.rs/banner/iimgg.jpg',
+    filename_poster: '',
+    filename_webm: '',
+    filename_ogv: '',
+    width: '100%',
+    height: '100',
+    headerHeight: '130',
+    clickTrack: 'https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjstmdDFc1ebmJqhg_yHS0rLhUTeDUM9wBCQ4PetgLSn54z5cKNYHhotX4J-KPstFl3aki8OwE_PdIc4PLg2H9ooMA-9iJ5tiYmWBpmrxqnWFusRcFc1hhLVvNqOFnlYGR8z4RmBIb3IcF1_49g7KY_PhzC5lXZM6Pd258R_BP2WLMrikFymrJn0hcW_KvOaomagI_oxK-3_COmT2XGhDBtuNf6N_UWuDlnzSFN4oJAHLJsHPb54o0UE&sig=Cg0ArKJSzL4y_CxRZHP6EAE&fbs_aeid=%5Bgw_fbsaeid%5D&urlfix=1&adurl=https://ad.doubleclick.net/ddm/trackclk/N6311.1987302ADRIAMEDIA/B31818828.391812918;dc_trk_aid=582991106;dc_trk_cid=212632861;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1',
+    impressor: ''
+};
+_zbfpq.push(cfg);
+
+(function () {
+    var e = document.createElement('script'); e.type = 'text/javascript'; e.async = true;
+    e.src = 'https://media.kurir.rs/banner/amz/js/zbf-pushdown.min5.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
+})();
+
